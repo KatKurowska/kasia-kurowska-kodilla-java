031187dee6115a31cdd40e8b534bde1bd2353845
KatKurowska <kata.kurowska@gmail.com>
2020-06-30 17:42:10 +0200

    7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,21 +1,40 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
 
-        Processor processor = new Processor();
-        Executor codeToExecute = () -> System.out.println("This is an example text.");
-        processor.execute(codeToExecute);
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //PoemBeautifier from ex.7.1
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Beautiful Poem written with ", (text) -> text + "ABC letters");
+        poemBeautifier.beautify("Beautiful Poem written with capital letters", (text) -> text.toUpperCase());
+        poemBeautifier.beautify("Beautiful Poem written with small letters", (text) -> text.toLowerCase());
+        poemBeautifier.beautify("Beautiful Poem written with no spaces", (text) -> text.replaceAll("\\s", ""));
     }
 }
-//Can be also in this way:
+
+//Can be done also in this way:
 /*public class StreamMain {
     public static void main(String[] args) {
         Processor processor = new Processor();
         processor.execute(() -> System.out.println("This is an example text."));
     }
 }*/
+
