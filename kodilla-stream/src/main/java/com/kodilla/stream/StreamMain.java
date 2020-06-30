a055bff94d7f49c55d197c0a7648121cb7969236
KatKurowska <kata.kurowska@gmail.com>
2020-06-30 13:01:05 +0200

    com.kodilla.stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,9 +1,21 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.Processor;
+
 public class StreamMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+
+        Processor processor = new Processor();
+        Executor codeToExecute = () -> System.out.println("This is an example text.");
+        processor.execute(codeToExecute);
     }
 }
-
+//Can be also in this way:
+/*public class StreamMain {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+    }
+}*/
