2d1fc68f26fc6d2823b3e07c148d8ab21821ddc9
KatKurowska <kata.kurowska@gmail.com>
2020-06-30 19:05:27 +0200

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -27,6 +28,9 @@ public class StreamMain {
         poemBeautifier.beautify("Beautiful Poem written with capital letters", (text) -> text.toUpperCase());
         poemBeautifier.beautify("Beautiful Poem written with small letters", (text) -> text.toLowerCase());
         poemBeautifier.beautify("Beautiful Poem written with no spaces", (text) -> text.replaceAll("\\s", ""));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
 
