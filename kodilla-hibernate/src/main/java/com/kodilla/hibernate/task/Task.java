6405ab4c2e96dfbba345a93e1cbd546b6d1e8b97
KatKurowska <kata.kurowska@gmail.com>
2021-03-11 12:24:10 +0100

    17.4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -6,6 +6,23 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
+@NamedQueries({
+        @NamedQuery(
+                name = "Task.retrieveLongTasks",
+                query = "FROM Task WHERE duration >10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveShortTasks",
+                query = "FROM Task WHERE duration <=10"
+        )
+})
+@NamedNativeQuery(
+        name = "Task.retrieveTasksWithEnoughTime",
+        query = "SELECT * FROM TASKS" +
+                "WHERE DATEDIFF (DATE_ADD (CREATED, INTERVAL DURATION DAY), NOW()) >5",
+        resultClass = Task.class
+)
+
 @Entity
 @Table(name = "TASKS")
 public class Task {
@@ -25,34 +42,39 @@ public class Task {
         this.created = new Date();
         this.duration = duration;
     }
+
     @Id
     @GeneratedValue
     @NotNull
-    @Column(name="ID", unique = true)
+    @Column(name = "ID", unique = true)
     public int getId() {
         return id;
     }
-    @Column(name="DESCRIPTION")
+
+    @Column(name = "DESCRIPTION")
     public String getDescription() {
         return description;
     }
+
     @NotNull
-    @Column(name="CREATED")
+    @Column(name = "CREATED")
     public Date getCreated() {
         return created;
     }
-    @Column(name="DURATION")
+
+    @Column(name = "DURATION")
     public int getDuration() {
         return duration;
     }
-    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-    @JoinColumn(name="TASKS_FINANCIALS_ID")
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "TASKS_FINANCIALS_ID")
     public TaskFinancialDetails getTaskFinancialDetails() {
         return taskFinancialDetails;
     }
 
     @ManyToOne
-    @JoinColumn(name="TASKLIST_ID")
+    @JoinColumn(name = "TASKLIST_ID")
     public TaskList getTaskList() {
         return taskList;
     }
